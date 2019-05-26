d76856ff4cb69681dae2151db69f1ecae7efa018
mitchell williams <mscottwilliams1994@gmail.com>
2019-05-26 02:12:16 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -10,3 +10,7 @@ function destructivelyAddElementToBeginningOfArray(array, element){
   array.unshift(element)
   return array 
 }
+function addElementToEndOfArray(array, element){
+  return [array, ...element]
+  
+}
