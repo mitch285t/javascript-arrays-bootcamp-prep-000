ba1afb38e8e9eb4cdcb21ae282e9d1be9de87505
mitchell williams <mscottwilliams1994@gmail.com>
2019-05-26 02:27:31 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -26,5 +26,8 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
 }
 function removeElementFromBeginningOfArray(array){
  return  array.slice(1)
-  
-}
+  }
+  function destructivelyRemoveElementFromEndOfArray(array){
+    array.pop()
+    return array 
+  }
