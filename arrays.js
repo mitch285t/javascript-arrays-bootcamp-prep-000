864c1f74755ce70f61807bedd251b9d7f2f8d507
mitchell williams <mscottwilliams1994@gmail.com>
2019-05-26 02:00:07 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,36 +1,9 @@
-var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+var chocolateBars = ["snickers, hundred grand, kitkat, skittles"];
 
-var array = [1];
-string = "foo"
-function addElementToBeginningOfArray(array, string){
-  return [string,...array]
-  }
-function destructivelyAddElementToBeginningOfArray(array, string){
-  array.unshift(string);
-  return array 
+
+function addElementToBeginningOfArray(array, element){
   
 }
-function addElementToEndOfArray(array, string){
-  return [...array, string]
-}
-function destructivelyAddElementToEndOfArray(array, string){
-   array.push(string)
-    return array
-  }
-function accessElementInArray(array, index){
-  return array[index]
-}
-function destructivelyRemoveElementFromBeginningOfArray(array){
-  array.shift()
-  return array
-}
-function removeElementFromBeginningOfArray(array) {
-  return array.slice(1); 
-}
-function destructivelyRemoveElementFromEndOfArray(array){
-  array.pop()
-  return array
-}
-function removeElementFromEndOfArray(array){
-  return array.slice(0, array.length - 1);
-}
+
+
+function destructivelyAddElementToBeginningOfArray(array, element)
