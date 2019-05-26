8cfb06dc3c338689d2f866c0a011ef6a81222ed3
mitchell williams <mscottwilliams1994@gmail.com>
2019-05-26 02:04:49 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -2,7 +2,7 @@ var chocolateBars = ["snickers, hundred grand, kitkat, skittles"];
 
 
 function addElementToBeginningOfArray(array, element){
-  ["foo", ...chocolateBars]
+  chocolateBars =["foo", ...chocolateBars]
   return chocolateBars
 }
 
