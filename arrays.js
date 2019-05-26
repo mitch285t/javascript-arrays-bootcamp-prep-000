d1a7484413575bec6d168106adabecec90434c2e
mitchell williams <mscottwilliams1994@gmail.com>
2019-05-26 02:22:10 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -20,3 +20,6 @@ function destructivelyAddElementToEndOfArray(array, element){
 function accessElementInArray(array, index){
   return array[index]
 }
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  return array.shift 
+}
